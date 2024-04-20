f035ed56ed4ab707c4ed96382bfcf9a01920da1c
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-04-20 20:39:15 +0200

    setup: xunit & Install Moq, apsnetcore.mvc.testing

---
+++ diversitytracker.api.tests/FormsControllerTests.cs
@@ -0,0 +1,15 @@
+using AutoMapper;
+using diversitytracker.api.Contracts;
+using diversitytracker.api.Controllers;
+using Moq;
+
+namespace diversitytracker.api.tests;
+
+public class FormsControllerTests
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
