cc0e608a9181e24bd5b1a1e88675c05e1268b32c
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-04-20 17:59:21 +0200

    add: FormsController -> implement GetFormResults

--- diversitytracker.api/Controllers/FormsController.cs
+++ diversitytracker.api/Controllers/FormsController.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using diversitytracker.api.Models;
 using diversitytracker.api.Models.Forms;
 using diversitytracker.api.Repository;
 using Microsoft.AspNetCore.Mvc;
@@ -12,17 +14,21 @@ namespace diversitytracker.api.Controllers
     [Route("api/[controller]")]
     public class FormsController : ControllerBase
     {
+        private readonly IMapper _mapper;
         private readonly FormsRepository _formsRepository;
 
-        public FormsController(FormsRepository formsRepository)
+        public FormsController(IMapper mapper, FormsRepository formsRepository)
         {
+            _mapper = mapper;
             _formsRepository = formsRepository;
         }
 
         [HttpGet]
         public async Task<ActionResult<List<BaseFormResponseDto>>> GetFormResults()
         {
-            return Ok();
+            var formsData = await _formsRepository.GetFormsAsync();
+            var formsResponseData = _mapper.Map<BaseFormResponseDto>(formsData);
+            return Ok(formsResponseData);
         }
 
         [HttpPost]
