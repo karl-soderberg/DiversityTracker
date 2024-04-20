b10bf266f6d54dccc653150edac0ab2babfa9d14
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-04-20 17:12:21 +0200

    init: .net webapi

---
+++ diversitytracker.api/Controllers/DataController.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace diversitytracker.api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DataController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetData()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostData()
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> PutData()
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteData()
+        {
+            return Ok();
+        }
+
+
+    }
+}
