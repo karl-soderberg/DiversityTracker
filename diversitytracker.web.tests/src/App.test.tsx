318ed34b2eec14bce1d26b94c50d257ec6063c6b
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-04-21 09:18:32 +0200

    refractor: tests

--- diversitytracker.web.tests/src/App.test.tsx
+++ diversitytracker.web.tests/src/App.test.tsx
@@ -3,22 +3,45 @@ import { render, fireEvent, screen } from '@testing-library/react';
 import App from 'src/App';
 
 vi.mock('src/pages/ChartPage', () => ({
-  ChartPage: ({ className }) => <div className={className}>Mocked Chart Page</div>
+  ChartPage: ({ className }) => <div className={className}>Chart Page</div>
 }));
 
 vi.mock('src/pages/FormPage', () => ({
-  FormPage: ({ className }) => <div className={className}>Mocked Form Page</div>
+  FormPage: ({ className }) => <div className={className}>Form Page</div>
 }));
 
 describe('App Component', () => {
   it('renders without crashing', () => {
+    // Arrange & Act
     const { container } = render(<App />);
+
+    // Assert
     expect(container).toBeTruthy();
   });
 
-  it('contains navigation bar', () => {
+  it('contains the navigation bar', () => {
+    // Arrange
     const { container } = render(<App />);
+
+    // Act
     const navbar = container.querySelector('.navbottom-container');
+
+    // Assert
     expect(navbar).toBeTruthy();
   });
+
+  it('changes state to ChartPage when clicking the charts view anchor', async () => {
+    // Arrange
+    const { container } = render(<App />);
+    const chartsAnchor = container.querySelector('.navbottom__item:first-child');
+
+    // Act
+    fireEvent.click(chartsAnchor);
+
+    // Assert
+    const chartPage = container.querySelector('.chartpage-container');
+    expect(chartPage).toHaveClass('chartpage-container active');
+  });
+
+  
 });
