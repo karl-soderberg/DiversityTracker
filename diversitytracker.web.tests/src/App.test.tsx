db7dcc7a6f43267e1a1ac1a49bf22801451a0083
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-04-21 09:14:06 +0200

    add test: contains navigation bar

--- diversitytracker.web.tests/src/App.test.tsx
+++ diversitytracker.web.tests/src/App.test.tsx
@@ -1,14 +1,24 @@
 import { describe, it, expect, vi } from 'vitest';
-import { render } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
 import App from 'src/App';
 
 vi.mock('src/pages/ChartPage', () => ({
   ChartPage: ({ className }) => <div className={className}>Mocked Chart Page</div>
 }));
 
+vi.mock('src/pages/FormPage', () => ({
+  FormPage: ({ className }) => <div className={className}>Mocked Form Page</div>
+}));
+
 describe('App Component', () => {
   it('renders without crashing', () => {
     const { container } = render(<App />);
     expect(container).toBeTruthy();
+  });
+
+  it('contains navigation bar', () => {
+    const { container } = render(<App />);
+    const navbar = container.querySelector('.navbottom-container');
+    expect(navbar).toBeTruthy();
   });
 });
