0a811ee92164bde4d051b3407d4335f0b5bc36d8
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-04-21 14:15:15 +0200

    update: MockData to incorporate mock DataResponseObject type

--- diversitytracker.web/src/data/MockData.ts
+++ diversitytracker.web/src/data/MockData.ts
@@ -1,1 +1,28 @@
-export const MockData = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400}, {name: 'Page B', uv: 300, pv: 1800, amt: 2400}, {name: 'Page B', uv: 500, pv: 500, amt: 500}]
+import { DataResponseObject } from "../types/types";
+
+export const MockData: DataResponseObject = {
+    men: [
+        { id: 1, value: 45 }, { id: 2, value: 29 }, { id: 3, value: 72 }, { id: 4, value: 19 }, { id: 5, value: 44 },
+        { id: 6, value: 88 }, { id: 7, value: 3 }, { id: 8, value: 41 }, { id: 9, value: 31 }, { id: 10, value: 76 },
+        { id: 11, value: 89 }, { id: 12, value: 5 }, { id: 13, value: 98 }, { id: 14, value: 75 }, { id: 15, value: 37 },
+        { id: 16, value: 18 }, { id: 17, value: 34 }, { id: 18, value: 47 }, { id: 19, value: 82 }, { id: 20, value: 70 },
+        { id: 21, value: 14 }, { id: 22, value: 92 }, { id: 23, value: 61 }, { id: 24, value: 8 }, { id: 25, value: 95 },
+        { id: 26, value: 68 }, { id: 27, value: 63 }, { id: 28, value: 38 }, { id: 29, value: 21 }, { id: 30, value: 49 },
+        { id: 31, value: 23 }, { id: 32, value: 13 }, { id: 33, value: 25 }, { id: 34, value: 53 }, { id: 35, value: 54 },
+        { id: 36, value: 51 }, { id: 37, value: 80 }, { id: 38, value: 60 }, { id: 39, value: 83 }, { id: 40, value: 77 },
+        { id: 41, value: 86 }, { id: 42, value: 16 }, { id: 43, value: 50 }, { id: 44, value: 26 }, { id: 45, value: 79 },
+        { id: 46, value: 42 }, { id: 47, value: 55 }, { id: 48, value: 87 }, { id: 49, value: 57 }, { id: 50, value: 100 }
+    ],
+    women: [
+        { id: 1, value: 65 }, { id: 2, value: 85 }, { id: 3, value: 9 }, { id: 4, value: 4 }, { id: 5, value: 2 },
+        { id: 6, value: 7 }, { id: 7, value: 58 }, { id: 8, value: 90 }, { id: 9, value: 17 }, { id: 10, value: 97 },
+        { id: 11, value: 20 }, { id: 12, value: 28 }, { id: 13, value: 93 }, { id: 14, value: 91 }, { id: 15, value: 27 },
+        { id: 16, value: 39 }, { id: 17, value: 10 }, { id: 18, value: 94 }, { id: 19, value: 64 }, { id: 20, value: 67 },
+        { id: 21, value: 66 }, { id: 22, value: 32 }, { id: 23, value: 43 }, { id: 24, value: 30 }, { id: 25, value: 96 },
+        { id: 26, value: 74 }, { id: 27, value: 59 }, { id: 28, value: 48 }, { id: 29, value: 33 }, { id: 30, value: 56 },
+        { id: 31, value: 6 }, { id: 32, value: 40 }, { id: 33, value: 84 }, { id: 34, value: 78 }, { id: 35, value: 11 },
+        { id: 36, value: 12 }, { id: 37, value: 99 }, { id: 38, value: 24 }, { id: 39, value: 15 }, { id: 40, value: 35 },
+        { id: 41, value: 36 }, { id: 42, value: 52 }, { id: 43, value: 81 }, { id: 44, value: 22 }, { id: 45, value: 46 },
+        { id: 46, value: 69 }, { id: 47, value: 71 }, { id: 48, value: 73 }, { id: 49, value: 62 }, { id: 50, value: 98 }
+    ]
+};
