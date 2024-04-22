68d57f7ee4c468b3a1f1dfcd30f5c8365c5c118c
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-04-22 22:27:57 +0200

    fix: Cycles error

--- diversitytracker.api/Program.cs
+++ diversitytracker.api/Program.cs
@@ -7,7 +7,10 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+.AddJsonOptions(options => {
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+});
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
