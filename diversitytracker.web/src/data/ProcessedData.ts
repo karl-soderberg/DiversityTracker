c042ce18c6342c1b6d82e88af82b722afd954162
karl-soderberg <118443902+karl-soderberg@users.noreply.github.com>
2024-04-22 14:43:59 +0200

    show procentage, instead of count in gender piegraph

--- diversitytracker.web/src/data/ProcessedData.ts
+++ diversitytracker.web/src/data/ProcessedData.ts
@@ -4,7 +4,11 @@ import { MOCKmay } from './MockData'
 const maleCount = MOCKmay.filter(entery => entery.gender === 'male').length;
 const femaleCount = MOCKmay.filter(entery => entery.gender === 'female').length;
 
+const totalCount = maleCount + femaleCount;
+const malePercentage = (maleCount/totalCount) * 100;
+const femalePercentage = (femaleCount/totalCount) * 100;
+
 export const pieData = [
-    { name: 'Male', value: maleCount },
-    { name: 'Female', value: femaleCount },
+    { name: 'Male', value: malePercentage },
+    { name: 'Female', value: femalePercentage },
 ];
