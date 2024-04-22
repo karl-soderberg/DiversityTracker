4843ee97000ff65735914135d419a82738005586
william00771 <william.f.lindberg@hotmail.com>
2024-04-22 14:33:56 +0200

    fix: Question model refeering back to FormsData

--- diversitytracker.api/Models/Question.cs
+++ diversitytracker.api/Models/Question.cs
@@ -1,11 +1,13 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace diversitytracker.api.Models
 {
     public class Question
     {
         public int Id { get; private set; }
-        public required QuestionType QuestionType { get; set; }
+        public int QuestionTypeId { get; set; }
         public required double Value { get; set; }
         public string? Answer { get; set; }
-        public required FormSubmission FormSubmission { get; set; }
+        // public required FormSubmission FormSubmission { get; set; }
     }
 }
