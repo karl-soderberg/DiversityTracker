517c490764376334b76e82aeba7cece1c3f09835
william00771 <william.f.lindberg@hotmail.com>
2024-04-22 13:18:37 +0200

    rename: FormsData -> FormSubmissionsData

--- diversitytracker.api/Models/FormSubmissionsData.cs
+++ diversitytracker.api/Models/FormSubmissionsData.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace diversitytracker.api.Models
 {
-    public class FormsData
+    public class FormSubmissionsData
     {
         public required ICollection<FormSubmission> FormSubmissions { get; set; }
         public DateTime RequestedAt { get; set; }
