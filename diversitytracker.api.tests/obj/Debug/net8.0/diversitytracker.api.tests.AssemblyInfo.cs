978d705086c75e6963b4c8e013cf3081419d2796
william00771 <william.f.lindberg@hotmail.com>
2024-04-22 12:59:28 +0200

    add: QuestionType model

--- diversitytracker.api.tests/obj/Debug/net8.0/diversitytracker.api.tests.AssemblyInfo.cs
+++ diversitytracker.api.tests/obj/Debug/net8.0/diversitytracker.api.tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("diversitytracker.api.tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80ba912ac332702bf6a798688b3ca0f3ab1bb4f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+465267259838d9382b9edacca50fcc1b681734f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("diversitytracker.api.tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("diversitytracker.api.tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ diversitytracker.api/Models/QuestionType.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace diversitytracker.api.Models
+{
+    public class QuestionType
+    {
+        public int Id { get; set; }
+        public string Value { get; set; }
+    }
+}
