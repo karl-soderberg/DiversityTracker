2e5652b4282582312cdda41200be69e166da6ac3
william00771 <william.f.lindberg@hotmail.com>
2024-04-23 16:14:35 +0200

    add: FormPage -> Questions Reflect form alternatives

--- diversitytracker.web/src/pages/FormPage.tsx
+++ diversitytracker.web/src/pages/FormPage.tsx
@@ -1,4 +1,7 @@
+import { useQuery } from 'react-query'
 import { CustomSlider } from '../components/CustomSlider'
+import { Question } from '../types/types'
+import { GetAllQuestions } from '../util/Http'
 import './FormPage.css'
 
 type Props = {
@@ -6,30 +9,33 @@ type Props = {
 }
 
 export const FormPage = ( {className} : Props) => {
+    const { data, isLoading, isError, error, refetch } = useQuery<Array<Question>, Error>({
+        queryKey: ['query'],
+        queryFn: () => GetAllQuestions()
+    });
 
     return(
         <section className={className}>
             <h1>Forms Page</h1>
             <p>Submit your form</p>
             <form className='formpage-container__form'>
-                <CustomSlider 
-                    min={0}
-                    max={100}
-                    step={.1}
-                    onChange={(value) => console.log(value)}
-                />
-                <CustomSlider 
-                    min={0}
-                    max={100}
-                    step={.1}
-                    onChange={(value) => console.log(value)}
-                />
-                <CustomSlider 
-                    min={0}
-                    max={100}
-                    step={.1}
-                    onChange={(value) => console.log(value)}
-                />
+                {isLoading && 'Loading...'}
+
+                {isError && 'Unknown Error occured...'}
+
+
+                {data && 
+                    data.map((question) => (
+                        <CustomSlider 
+                            min={0}
+                            max={100}
+                            step={.1}
+                            onChange={(value) => console.log(value)}
+                            text={question.value}
+                            key={question.id}
+                        />
+                    ))
+                }
                 <button 
                     className='btn-primary--gradient-outline form-input__buttonlogin'
                     type='button'
