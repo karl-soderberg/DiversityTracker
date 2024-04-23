ccad04dae5f2e3830374d0fdfaf435148bda686b
william00771 <william.f.lindberg@hotmail.com>
2024-04-23 10:21:28 +0200

    add: Http -> GetAllQuestions

--- diversitytracker.web/src/util/Http.ts
+++ diversitytracker.web/src/util/Http.ts
@@ -1,2 +1,15 @@
 const API_URL = import.meta.env.VITE_API_URL;
 
+export const GetAllQuestions = async () => {
+    const response = await fetch(`${API_URL}/Questions`);
+
+    if (!response.ok) {
+        const error = new Error('An error occurred while fetching questions');
+        error.message = await response.json();
+        throw error;
+    }
+
+    const questions = await response.json();
+
+    return questions;
+}
