8d22c04577ef510fc2702059ff6ef08b2bca11e1
william00771 <william.f.lindberg@hotmail.com>
2024-04-23 10:17:54 +0200

    add: env variable -> API_URL

--- diversitytracker.web/src/pages/AdminPage.tsx
+++ diversitytracker.web/src/pages/AdminPage.tsx
@@ -9,6 +9,12 @@ export const AdminPage = ( {className} : Props) => {
     return(
         <section className={className}>
             <h1>Admin Page</h1>
+            <article>
+                <h2>FormData Questions</h2>
+                <ul>
+                    <li>question1</li>
+                </ul>
+            </article>
         </section>
     )
 }
