89b6f3370e9e32822b1bbbba6a1ebf3a264de142
karl-soderberg <118443902+karl-soderberg@users.noreply.github.com>
2024-04-24 12:48:54 +0200

    feat: get user info in frontend

--- diversitytracker.web/src/App.tsx
+++ diversitytracker.web/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import './App.css'
 import { NavBottom } from "./shared_pages/NavBottom";
 import { ChartPage } from "./pages/ChartPage";
@@ -7,6 +7,25 @@ import { NewFormPage } from "./pages/NewFormPage";
 
 function App() {
   const [page, setPage] = useState("ChartPage");
+  const [userInfo, setUserInfo] = useState();
+
+  useEffect(() => {
+    (async () => {
+      setUserInfo(await getUserInfo());
+    })();
+  }, []);
+
+  async function getUserInfo() {
+    try {
+      const response = await fetch('/.auth/me');
+      const payload = await response.json();
+      const { clientPrincipal } = payload;
+      return clientPrincipal;
+    } catch (error) {
+      console.error('No profile could be found');
+      return undefined;
+    }
+  }
 
   return (
     <>
