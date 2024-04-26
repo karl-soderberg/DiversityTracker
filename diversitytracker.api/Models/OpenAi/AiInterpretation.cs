c9fe44086097c48977ecdc956f9089ff7cebb30c
william00771 <william.f.lindberg@hotmail.com>
2024-04-26 13:07:06 +0200

    update: AIInterpretation model

--- diversitytracker.api/Models/OpenAi/AiInterpretation.cs
+++ diversitytracker.api/Models/OpenAi/AiInterpretation.cs
@@ -3,6 +3,8 @@ namespace diversitytracker.api.Models.OpenAi
     public class AiInterpretation
     {
         public string Id { get; private set; } = "AiInter" + Ulid.NewUlid().ToString();
+        public string QuestionTypeId { get; set; }
+        public required QuestionType QuestionType { get; set; }
         public string RealDataInterpretation { get; set; }
         public string ReflectionsInterpretation { get; set; }
         public int[] ReflectionValuesInterpretation { get; set; }
