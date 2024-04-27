da30c5ecbab2612f5e4611be819fcb0a39d8dc32
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-04-27 23:24:58 +0200

    add: AiInterpretationController -> implement Endpoints

--- diversitytracker.api/Controllers/AiInterpretationController.cs
+++ diversitytracker.api/Controllers/AiInterpretationController.cs
@@ -1,3 +1,4 @@
+using diversitytracker.api.Contracts;
 using diversitytracker.api.Models.OpenAi;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,28 +8,95 @@ namespace diversitytracker.api.Controllers
     [Route("api/[controller]")]
     public class AiInterpretationController : ControllerBase
     {
+        private readonly IAiInterpretationService _aiInterpretationService;
+        private readonly IFormsRepository _formsRepository;
+        private readonly IQuestionsRepository _questionsRepository;
+        private readonly IAiInterpretationRepository _aiInterpretationRepository;
+
+        public AiInterpretationController(IAiInterpretationService aiInterpretationService,IFormsRepository formsRepository, IQuestionsRepository questionsRepository, IAiInterpretationRepository aiInterpretationRepository)
+        {
+            _aiInterpretationService = aiInterpretationService;
+            _formsRepository = formsRepository;
+            _questionsRepository = questionsRepository;
+            _aiInterpretationRepository = aiInterpretationRepository;
+        }
+        [HttpGet] 
+        public async Task<ActionResult<AiInterpretation>> GetInterperetation()
+        {
+            var interperetation = await _aiInterpretationRepository.GetAiInterpretationAsync();
+            if(interperetation == null)
+            {
+                return NotFound();
+            }
+            return Ok(interperetation);
+        }
+
         [HttpGet("InterperetAllReflectionsForms")]
         public async Task<ActionResult<AiInterpretation>> InterperetAllReflectionsForms()
         {
-            return Ok();
+            try
+            {
+                var forms = await _formsRepository.GetFormsAsync(null, null);
+                var questionTypes = await _questionsRepository.GetQuestionTypesAsync();
+                var interperetation = await _aiInterpretationService.InterperetAllReflectionsFormsAsync(forms, questionTypes);
+                return Ok(interperetation);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return StatusCode(500, "Internal Server Error");
+            }   
         }
 
         [HttpGet("InterperetAllRealData")]
         public async Task<ActionResult<AiInterpretation>> InterperetAllRealData()
         {
-            return Ok();
+            try
+            {
+                var forms = await _formsRepository.GetFormsAsync(null, null);
+                var questionTypes = await _questionsRepository.GetQuestionTypesAsync();
+                var interperetation = await _aiInterpretationService.InterperetAllRealDataAsync(forms, questionTypes);
+                return Ok(interperetation);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return StatusCode(500, "Internal Server Error");
+            }  
         }
 
         [HttpGet("InterperetAllQuestions")]
         public async Task<ActionResult<AiInterpretation>> InterperetAllQuestions()
         {
-            return Ok();
+            try
+            {
+                var forms = await _formsRepository.GetFormsAsync(null, null);
+                var questionTypes = await _questionsRepository.GetQuestionTypesAsync();
+                var interperetation = await _aiInterpretationService.InterperetAllQuestionsAsync(forms, questionTypes);
+                return Ok(interperetation);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return StatusCode(500, "Internal Server Error");
+            }  
         }
 
         [HttpGet("InterperetRealDataSeperated")]
         public async Task<ActionResult<AiInterpretation>> InterperetRealData()
         {
-            return Ok();
+            try
+            {
+                var forms = await _formsRepository.GetFormsAsync(null, null);
+                var questionTypes = await _questionsRepository.GetQuestionTypesAsync();
+                var interperetation = await _aiInterpretationService.InterperetRealDataSeperatedAsync(forms, questionTypes);
+                return Ok(interperetation); 
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return StatusCode(500, "Internal Server Error");
+            }  
         }
     }
 }
