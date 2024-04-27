427db189f5ab66fb656520f50ef8d3062dcc06b4
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-04-27 22:17:07 +0200

    add: AIInterpretationRepository -> implement GetAiInterpretationAsync

--- diversitytracker.api/Repository/AiInterpretationRepository.cs
+++ diversitytracker.api/Repository/AiInterpretationRepository.cs
@@ -1,16 +1,24 @@
 using diversitytracker.api.Contracts;
+using diversitytracker.api.Data;
 using diversitytracker.api.Models.OpenAi;
+using Microsoft.EntityFrameworkCore;
 
 namespace diversitytracker.api.Repository
 {
     public class AiInterpretationRepository : IAiInterpretationRepository
     {
-        public Task<List<AiInterpretation>> GetAiInterpretationAsync()
+        private readonly AppDbContext _context;
+
+        public AiInterpretationRepository(AppDbContext context)
         {
-            throw new NotImplementedException();
+            _context = context;
+        }
+        public async Task<List<AiInterpretation>> GetAiInterpretationAsync()
+        {
+            return await _context.AiInterpretations.ToListAsync();
         }
 
-        public Task UpdateAiInterpretationAsync(AiInterpretation questionType)
+        public async Task UpdateAiInterpretationAsync(AiInterpretation questionType)
         {
             throw new NotImplementedException();
         }
