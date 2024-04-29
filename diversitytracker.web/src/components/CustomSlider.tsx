3e2b2cdc16b336cf32ab0e767a6f779f7ed219a8
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-04-29 23:12:10 +0200

    setup: lottie

--- diversitytracker.web/src/components/CustomSlider.tsx
+++ diversitytracker.web/src/components/CustomSlider.tsx
@@ -1,4 +1,4 @@
-import {useState } from 'react';
+import {useEffect, useRef, useState } from 'react';
 import './CustomSlider.css'
 
 type SliderProps = {
@@ -11,7 +11,8 @@ type SliderProps = {
 
 export const CustomSlider = ({min, max, step, text, onChange }: SliderProps) => {
     const [value, setValue] = useState((max + min) / 2 - 1.8);
-    const [compensatorValue, setCompensatorValue] = useState(4.7);
+    const [compensatorValue, setCompensatorValue] = useState(2);
+    const emotionIconRef = useRef(null);
 
     const handleSliderChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const newValue = Number(event.target.value);
@@ -28,9 +29,10 @@ export const CustomSlider = ({min, max, step, text, onChange }: SliderProps) => 
             <label className='slider__label' htmlFor="">{text + " " + Math.round(value / 10)}</label>
             <div className='slider__overlay' style={{ left: `${value+compensatorValue}%`, transform: `translateX(-50%)`}}>
                 {/* {value} */}
-                <svg viewBox="0 0 15 15" version="1.1" id="marker" xmlns="http://www.w3.org/2000/svg">
+                {/* <svg viewBox="0 0 15 15" version="1.1" id="marker" xmlns="http://www.w3.org/2000/svg">
                     <path id="path4133" d="M7.5,0C5.0676,0,2.2297,1.4865,2.2297,5.2703&#xA;&#x9;C2.2297,7.8378,6.2838,13.5135,7.5,15c1.0811-1.4865,5.2703-7.027,5.2703-9.7297C12.7703,1.4865,9.9324,0,7.5,0z"/>
-                </svg>
+                </svg> */}
+            
             </div>
             <input
                 type="range"
