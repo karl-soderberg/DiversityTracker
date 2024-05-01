264d998ce377c1bc1ae1bbe54de686d11b77603f
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-05-01 22:43:53 +0200

    bugfixing: Mobile usage bug fixes

--- diversitytracker.api.tests/obj/Debug/net8.0/diversitytracker.api.tests.AssemblyInfo.cs
+++ diversitytracker.api.tests/obj/Debug/net8.0/diversitytracker.api.tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("diversitytracker.api.tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8c26d2ce6842a047e29fafd5dcac625e398962f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64a774514794ffefd97d5cdb1d08607ee77cbe12")]
 [assembly: System.Reflection.AssemblyProductAttribute("diversitytracker.api.tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("diversitytracker.api.tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
