e78d8e86d531d3d63b53311b84567f0f39b92562
karl-soderberg <118443902+karl-soderberg@users.noreply.github.com>
2024-05-02 10:12:21 +0200

    update mockdata

--- diversitytracker.web/src/data/MockData.ts
+++ diversitytracker.web/src/data/MockData.ts
@@ -1,14 +1,14 @@
 export const MOCKData = [
-    { value: 1, numberofmen: 2, numberofwomen: 10, date: `okt` },
-    { value: 2, numberofmen: 5, numberofwomen: 17, date: `nov` },
-    { value: 3, numberofmen: 7, numberofwomen: 25, date: `dec` },
-    { value: 4, numberofmen: 12, numberofwomen: 20, date: `jan` },
-    { value: 5, numberofmen: 17, numberofwomen: 18, date: `feb` },
-    { value: 6, numberofmen: 25, numberofwomen: 15, date: `mar` },
-    { value: 7, numberofmen: 10, numberofwomen: 10, date: `apr` },
-    { value: 8, numberofmen: 5, numberofwomen: 5, date: `may` },
-    { value: 9, numberofmen: 2, numberofwomen: 2, date: `june` },
-    { value: 10, numberofmen: 1, numberofwomen: 2, date: `july` }
+  { value: 1, numberofmen: 4, numberofwomen: 3, date: `okt` },
+  { value: 2, numberofmen: 5, numberofwomen: 4, date: `nov` },
+  { value: 3, numberofmen: 4, numberofwomen: 5, date: `dec` },
+  { value: 4, numberofmen: 3, numberofwomen: 3, date: `jan` },
+  { value: 5, numberofmen: 5, numberofwomen: 3, date: `feb` },
+  { value: 6, numberofmen: 5, numberofwomen: 5, date: `mar` },
+  { value: 7, numberofmen: 6, numberofwomen: 7, date: `apr` },
+  { value: 8, numberofmen: 6, numberofwomen: 9, date: `may` },
+  { value: 9, numberofmen: 7, numberofwomen: 10, date: `june` },
+  { value: 10, numberofmen: 8, numberofwomen: 9, date: `july` }
 ];
 
 export const MOCKmay = [
