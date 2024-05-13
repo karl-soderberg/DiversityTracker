f9c24f4b0c41c4b99ca76e20d5afc8f45e5c9c97
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-05-13 11:18:37 +0200

    refractor: PromtService -> Convert all dictionary based prompts into one generic

--- diversitytracker.api/Contracts/IPromptService.cs
+++ diversitytracker.api/Contracts/IPromptService.cs
@@ -3,10 +3,6 @@ namespace diversitytracker.api.Contracts
     public interface IPromptService
     {
         string CreateReflectionAnswersDataPrompt(List<string> reflectionAnswerData);
-        string CreateRealdataPrompt(Dictionary<string, double[]> realData);
-        string CreateRealdataMultiplePrompt(Dictionary<string, double[]> realData);
-        string CreateQuestionAnswersDataPrompt(Dictionary<string, string[]> questionAnswerData);
-        string CreateDataFromQuestionAnswersPrompt(Dictionary<string, string[]> questionAnswerData);
-        string CountQuestionWordLengthPrompt(Dictionary<string, string[]> questionAnswerData);
+        string CreatePromptFromDictionary<T>(Dictionary<string, T[]> data, string header, string itemPrefix, string sectionSeparator, string sectionHeader, Func<T, string> itemFormatter);
     }
 }
--- diversitytracker.api/Services/AiInterpretationService.cs
+++ diversitytracker.api/Services/AiInterpretationService.cs
@@ -105,7 +105,11 @@ namespace diversitytracker.api.Services
                 }
             }
 
-            var realDataPrompt = _promptService.CreateRealdataPrompt(realData);
+            var realDataPrompt = _promptService.CreatePromptFromDictionary(realData,
+                "Here is a collection of questions and answers where people ranked 0-10. The Question and answers section is seperated by || \n I want you to draw real world conclusions about the data more highlighting the emotional/personal points based on the data. Do not give answer on the data values themselves. Answer in under 50 words.\n\n",
+                "- ", "->-", "", 
+                value => Math.Round(value).ToString());;
+
             var realDataInterpretation = await OpenAIInterperet(realDataPrompt);
 
             var aiInterpretation = await _aiInterpretationRepository.GetAiInterpretationAsync();
@@ -148,7 +152,10 @@ namespace diversitytracker.api.Services
                 }
             }
 
-            var realDataSeperatedPrompt = _promptService.CreateRealdataMultiplePrompt(realData);
+            var realDataSeperatedPrompt = _promptService.CreatePromptFromDictionary(realData,
+                $"Here is a collection of questions and answers where many individuals ranked 0-10. The Question and answers section is seperated by || \n I want you to reflect on what people have answered and draw meaningful insights about the data more highlighting the emotional and interpersonal conclusions based on the data. Draw on the negative and positive points. Every section is seperated by ->- . I want you to give one answer per section and seperate the answers by two new lines. Don't give an answer on the data values themselves. Answer in between 100-120 words for each question/answers. Only give me a text no bullet points or similar. It's Important that you seperate YOUR ANSWERS with two new lines.!\n\n",
+                "", "->-", "", value => Math.Round(value).ToString());
+                
             var realDataSeperatedInterpretation = await OpenAIInterperet(realDataSeperatedPrompt);
             var realDataSeperatedInterpretations = realDataSeperatedInterpretation.Split(new string[] { "\n\n" }, StringSplitOptions.RemoveEmptyEntries);
 
@@ -232,7 +239,10 @@ namespace diversitytracker.api.Services
                 }
             }
 
-            var questionAnswerPrompt = _promptService.CreateQuestionAnswersDataPrompt(questionAnswersData);
+            var questionAnswerPrompt = _promptService.CreatePromptFromDictionary(questionAnswersData,
+                $"Here is a collection of questions with answers from people working at an organization. It's Important that you seperate YOUR ANSWERS with two new lines! The Question contra answer inside a section is seperated by ||. The sections of one question/answer are seperated by ->- \n I want you to draw real world conclusions about the answers related to the given question more highlighting the problem areas in the organization but also some objective conclusions. Give a 85-120 word answer for each question/answers section. It's Important that you seperate YOUR ANSWERS with two new lines.!\n\n",
+                "- ", "->- \n", "Question: ", value => value);
+                
             var questionAnswerInterpretation = await OpenAIInterperet(questionAnswerPrompt);
             var questionAnswerInterpretations = questionAnswerInterpretation.Split(new string[] { "\n\n" }, StringSplitOptions.RemoveEmptyEntries);
 
@@ -312,8 +322,13 @@ namespace diversitytracker.api.Services
                 }
             }
 
-            var questionDataPrompt = _promptService.CreateDataFromQuestionAnswersPrompt(questionAnswersData);
-            var questionWordLengthPrompt = _promptService.CountQuestionWordLengthPrompt(questionAnswersData);
+            var questionDataPrompt = _promptService.CreatePromptFromDictionary(questionAnswersData,
+                $"Here is a collection of questions with answers from people working at an organization. It's Important that you seperate YOUR ANSWERS with two new lines! Do not include the question itself JUST the arrays! The Question and answers section is seperated by || \n . I want you to write an array of values 0-10 for each persons answer and bundle them into one array per question. 0 being poor, 10 being really good opinion. The sections are seperated by ->-. It's Important that you seperate YOUR ANSWERS with two new lines.!\n\n",
+                "- ", "->- \n", "Question: ", value => value);
+
+            var questionWordLengthPrompt = _promptService.CreatePromptFromDictionary(questionAnswersData,
+                $"I want you to write an array of number of words contained in each persons answer and bundle them into one array per question. Here is a collection of questions with answers from people working at an organization. It's Important that you seperate YOUR ANSWERS with two new lines! The Question and answers section is seperated by || \n .  The sections are seperated by ->- . It's Important that you seperate YOUR ANSWERS with two new lines.!\n\n",
+                "", "->- \n", "Question: ", value => value.Split(' ').Length.ToString());
 
             var questionDataAiInterpretation = await OpenAIInterperet(questionDataPrompt);
             var questionWordLengthAiInterpretation = await OpenAIInterperet(questionWordLengthPrompt);
